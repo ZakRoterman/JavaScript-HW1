4459f7d3f98ab552017fc1e9036dc561a0d5e312
ZakRoterman <97009972+ZakRoterman@users.noreply.github.com>
2022-02-23 18:08:33 -0500

    Added Codewars problems

--- hw.js
+++ hw.js
@@ -40,3 +40,43 @@ console.log(replaceEvens(arr))
 //Expected output
 //Given arr == ["Max","Baseball","Reboot","Goku","Trucks","Rodger"]
 //Output arr == ["even index","Baseball","even index","Goku","even index","Rodger"]
+
+// # In this simple assignment you are given a number and have to make it negative. 
+// # But maybe the number is already negative?
+
+// python code:
+// def make_negative(x):
+//     return -abs(x)
+
+const n1 = 5;
+const n2 = 7;
+const n3 = -5;
+const n4 = 100;
+
+function makeNegative(x){
+    console.log(-Math.abs(x))
+};
+
+makeNegative(n4)
+
+// Square N Sum
+// # Complete the square sum function so that it squares each number passed into 
+// # it and then sums the results together.
+
+// def square_sum(numbers):
+//     sum = 0
+//     for num in numbers:
+//         sum+= num**2
+//     return sum
+
+let numbers = [5,2]
+
+function squareSum(numbers){
+    let sum = 0;
+    for (let num of numbers){
+        sum+= num**2
+    };
+    return sum
+};
+
+console.log(squareSum(numbers))
